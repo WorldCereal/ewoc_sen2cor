e54b72db9cce98372c0dbeab56f4fd7718f01777
Benatia Fahd <fahd.benatia@csgroup.eu>
2021-07-01 14:53:36 +0200

    update dockerfile and run_s2c.py

--- run_s2c.py
+++ run_s2c.py
@@ -1,37 +1,17 @@
 import os
 import json
+import shutil
 
 import click
 
+from utils import run_s2c, clean, last_safe
 from dataship.dag.utils import l2a_to_ard, get_product_by_id
-
-
-
-def run_s2c(l1c_safe,l2a_out):
-    s2c_cmd = f"./Sen2Cor-02.09.00-Linux64/bin/L2A_Process {l1c_safe} --output_dir {l2a_out} --resolution 10"
-    os.system(s2c_cmd)
-    l2a_safe_folder = [os.path.join(l2a_out, fold) for fold in os.listdir(l2a_out) if fold.endswith('SAFE')][0]
-    return l2a_safe_folder
-def clean(folder):
-    os.system(f"rm -r {folder}")
-def last_safe(safe_folder):
-    """
-    Get the deepest/last SAFE folder when there are many
-    :param safe_folder: path to .SAFE folder
-    :return: last path
-    """
-    tmp = safe_folder
-    for root, dirs, files in os.walk(safe_folder):
-        for dir in dirs:
-            if dir.endswith('SAFE'):
-                tmp = os.path.join(root,dir)
-    return tmp
 
 @click.group()
 def cli():
     click.secho("Run sen2cor", fg="green",bold=True)
 
-@cli.command('s2c',help="Sen2cor")
+@cli.command('s2c_plan',help="Sen2cor")
 @click.option('-p', '--plan', help="EWoC Plan in json format")
 @click.option('-o', '--l2a_dir', help="Output directory")
 @click.option('-cfg', '--config', help="EOdag config file")
@@ -40,21 +20,49 @@ def run_plan(plan, l2a_dir,provider,config):
     with open(plan) as f:
         plan = json.load(f)
     for tile in plan:
-        out_dir = os.path.join(l2a_dir,"L1C",tile)
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
         prods = plan[tile]['S2_PROC']['INPUTS']
-        for prod in prods[:3]:
-            get_product_by_id(prod['id'], out_dir, provider, config_file=config)
-            l1c_safe_folder = [os.path.join(out_dir, fold) for fold in os.listdir(out_dir) if fold.endswith('SAFE')][0]
+        for prod in prods[:2]:
+            out_dir_l1c = os.path.join(l2a_dir, "tmp_L1C", tile)
+            out_dir_l2a = os.path.join(l2a_dir, "tmp_L2A", tile)
+            if not os.path.exists(out_dir_l1c):
+                os.makedirs(out_dir_l1c)
+            if not os.path.exists(out_dir_l2a):
+                os.makedirs(out_dir_l2a)
+            get_product_by_id(prod['id'], out_dir_l1c, provider, config_file=config)
+            l1c_safe_folder = [os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith('SAFE')][0]
             l1c_safe_folder = last_safe(l1c_safe_folder)
+
             print(f'Download done for {prod["id"]}\n')
             try:
-                l2a_safe_folder = run_s2c(l1c_safe_folder,l2a_dir)
+                l2a_safe_folder = run_s2c(l1c_safe_folder,out_dir_l2a)
                 l2a_to_ard(l2a_safe_folder,l2a_dir)
-                clean(l2a_safe_folder)
-                clean(l1c_safe_folder)
+                clean(out_dir_l2a)
+                clean(out_dir_l1c)
             except:
                 print(f'Something went wrong with {prod["id"]}')
+@cli.command('s2c_id',help="Sen2cor")
+@click.option('-p', '--pid', help="S2 L1C product ID")
+@click.option('-o', '--l2a_dir', help="Output directory")
+@click.option('-cfg', '--config', help="EOdag config file")
+@click.option('-pv', '--provider', default = "creodias",help="Data provider")
+def run_id(pid, l2a_dir,provider,config):
+        out_dir_l1c = os.path.join(l2a_dir, "tmp_L1C")
+        out_dir_l2a = os.path.join(l2a_dir, "tmp_L2A")
+        if not os.path.exists(out_dir_l1c):
+            os.makedirs(out_dir_l1c)
+        if not os.path.exists(out_dir_l2a):
+            os.makedirs(out_dir_l2a)
+        get_product_by_id(pid, out_dir_l1c, provider, config_file=config)
+        l1c_safe_folder = [os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith('SAFE')][0]
+        l1c_safe_folder = last_safe(l1c_safe_folder)
+
+        print(f'Download done for {pid}\n')
+        try:
+            l2a_safe_folder = run_s2c(l1c_safe_folder,out_dir_l2a)
+            l2a_to_ard(l2a_safe_folder,l2a_dir)
+            clean(out_dir_l2a)
+            clean(out_dir_l1c)
+        except:
+            print(f'Something went wrong with {pid}')
 if __name__ == "__main__":
     cli()
---
+++ utils.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+def run_s2c(l1c_safe,l2a_out):
+    """
+    Run sen2cor subprocess
+    :param l1c_safe: Path to SAFE folder
+    :param l2a_out: Path to output directory for generated L2A products
+    :return: Path to L2A SAFE
+    """
+    # L2A_Process is expected to be added to /bin/
+    # After installing sen2cor run source Sen2Cor-02.09.00-Linux64/L2A_Bashrc
+    # This should work in container and local env
+    s2c_cmd = f"L2A_Process {l1c_safe} --output_dir {l2a_out} --resolution 10"
+    os.system(s2c_cmd)
+    l2a_safe_folder = [os.path.join(l2a_out, fold) for fold in os.listdir(l2a_out) if fold.endswith('SAFE')][0]
+    return l2a_safe_folder
+
+def clean(folder):
+    shutil.rmtree(folder)
+
+def last_safe(safe_folder):
+    """
+    Get the deepest/last SAFE folder when there are many
+    :param safe_folder: path to .SAFE folder
+    :return: last path
+    """
+    tmp = safe_folder
+    for root, dirs, files in os.walk(safe_folder):
+        for dir in dirs:
+            if dir.endswith('SAFE'):
+                tmp = os.path.join(root,dir)
+    return tmp
