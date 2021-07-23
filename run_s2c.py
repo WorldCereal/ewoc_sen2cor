8adc02ceea84157b3bb936a25b85a68cac6e22c2
Benatia Fahd <fahd.benatia@csgroup.eu>
2021-07-23 13:49:25 +0200

    add get_prod_by_id to try except

--- run_s2c.py
+++ run_s2c.py
@@ -28,12 +28,12 @@ def run_plan(plan, l2a_dir,provider,config):
                 os.makedirs(out_dir_l1c)
             if not os.path.exists(out_dir_l2a):
                 os.makedirs(out_dir_l2a)
-            get_product_by_id(prod['id'], out_dir_l1c, provider, config_file=config)
-            l1c_safe_folder = [os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith('SAFE')][0]
-            l1c_safe_folder = last_safe(l1c_safe_folder)
-
             print(f'Download done for {prod["id"]}\n')
             try:
+                get_product_by_id(prod['id'], out_dir_l1c, provider, config_file=config)
+                l1c_safe_folder = \
+                [os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith('SAFE')][0]
+                l1c_safe_folder = last_safe(l1c_safe_folder)
                 l2a_safe_folder = run_s2c(l1c_safe_folder,out_dir_l2a)
                 l2a_to_ard(l2a_safe_folder,l2a_dir)
                 clean(out_dir_l2a)
@@ -52,12 +52,12 @@ def run_id(pid, l2a_dir,provider,config):
             os.makedirs(out_dir_l1c)
         if not os.path.exists(out_dir_l2a):
             os.makedirs(out_dir_l2a)
-        get_product_by_id(pid, out_dir_l1c, provider, config_file=config)
-        l1c_safe_folder = [os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith('SAFE')][0]
-        l1c_safe_folder = last_safe(l1c_safe_folder)
-
         print(f'Download done for {pid}\n')
         try:
+            get_product_by_id(pid, out_dir_l1c, provider, config_file=config)
+            l1c_safe_folder = \
+            [os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith('SAFE')][0]
+            l1c_safe_folder = last_safe(l1c_safe_folder)
             l2a_safe_folder = run_s2c(l1c_safe_folder,out_dir_l2a)
             l2a_to_ard(l2a_safe_folder,l2a_dir)
             clean(out_dir_l2a)
