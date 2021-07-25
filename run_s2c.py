3401e5a75b3722b2c50a5eca35b737c3e15c0ae0
Benatia Fahd <fahd.benatia@csgroup.eu>
2021-07-25 20:46:03 +0200

    update dataship and add products count

--- run_s2c.py
+++ run_s2c.py
@@ -6,6 +6,7 @@ import click
 
 from utils import run_s2c, clean, last_safe
 from dataship.dag.utils import l2a_to_ard, get_product_by_id
+from dataship.dag.s3man import recursive_upload_dir_to_s3
 
 @click.group()
 def cli():
@@ -17,6 +18,7 @@ def cli():
 @click.option('-cfg', '--config', help="EOdag config file")
 @click.option('-pv', '--provider', default = "creodias",help="Data provider")
 def run_plan(plan, l2a_dir,provider,config):
+    count = 0
     with open(plan) as f:
         plan = json.load(f)
     for tile in plan:
@@ -35,11 +37,18 @@ def run_plan(plan, l2a_dir,provider,config):
                 [os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith('SAFE')][0]
                 l1c_safe_folder = last_safe(l1c_safe_folder)
                 l2a_safe_folder = run_s2c(l1c_safe_folder,out_dir_l2a)
-                l2a_to_ard(l2a_safe_folder,l2a_dir)
+                ard_folder = l2a_to_ard(l2a_safe_folder,l2a_dir)
+                print(ard_folder)
                 clean(out_dir_l2a)
                 clean(out_dir_l1c)
+                count=+1
             except:
                 print(f'Something went wrong with {prod["id"]}')
+    number_of_products = len(prods)
+    print('\n\nEnd of processing ')
+    # Check if
+    print(f'Processed {str(count)} of {str(number_of_products)}')
+
 @cli.command('s2c_id',help="Sen2cor for on product using EOdag ID")
 @click.option('-p', '--pid', help="S2 L1C product ID")
 @click.option('-o', '--l2a_dir', help="Output directory")
@@ -52,17 +61,17 @@ def run_id(pid, l2a_dir,provider,config):
             os.makedirs(out_dir_l1c)
         if not os.path.exists(out_dir_l2a):
             os.makedirs(out_dir_l2a)
-        try:
-            get_product_by_id(pid, out_dir_l1c, provider, config_file=config)
-            print(f'Download done for {pid}\n')
-            l1c_safe_folder = \
-            [os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith('SAFE')][0]
-            l1c_safe_folder = last_safe(l1c_safe_folder)
-            l2a_safe_folder = run_s2c(l1c_safe_folder,out_dir_l2a)
-            l2a_to_ard(l2a_safe_folder,l2a_dir)
-            clean(out_dir_l2a)
-            clean(out_dir_l1c)
-        except:
-            print(f'Something went wrong with {pid}')
+        get_product_by_id(pid, out_dir_l1c, provider, config_file=config)
+        print(f'Download done for {pid}\n')
+        l1c_safe_folder = \
+        [os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith('SAFE')][0]
+        l1c_safe_folder = last_safe(l1c_safe_folder)
+        l2a_safe_folder = run_s2c(l1c_safe_folder,out_dir_l2a)
+        ard_folder = l2a_to_ard(l2a_safe_folder,l2a_dir)
+        print(ard_folder)
+        clean(out_dir_l2a)
+        clean(out_dir_l1c)
+
+
 if __name__ == "__main__":
     cli()
