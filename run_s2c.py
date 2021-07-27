d587369e65cd01d310c521f6b474285ba8240591
Benatia Fahd <fahd.benatia@gmail.com>
2021-07-27 16:20:33 +0200

    new s2c_db template

--- run_s2c.py
+++ run_s2c.py
@@ -14,10 +14,12 @@ def cli():
 
 @cli.command('s2c_plan', help="Sen2cor with a full plan as input")
 @click.option('-p', '--plan', help="EWoC Plan in json format")
-@click.option('-o', '--l2a_dir', help="Output directory")
-@click.option('-cfg', '--config', help="EOdag config file")
-@click.option('-pv', '--provider', default="creodias", help="Data provider")
+@click.option('-o', '--l2a_dir', default=None,help="Output directory")
+@click.option('-cfg', '--config', default=None,help="EOdag config file")
+@click.option('-pv', '--provider', default=None, help="Data provider")
 def run_plan(plan, l2a_dir, provider, config):
+    if l2a_dir is None:
+        l2a_dir = "/work/SEN2TEST/OUT/"
     count = 0
     with open(plan) as f:
         plan = json.load(f)
@@ -51,10 +53,12 @@ def run_plan(plan, l2a_dir, provider, config):
 
 @cli.command('s2c_id', help="Sen2cor for on product using EOdag ID")
 @click.option('-p', '--pid', help="S2 L1C product ID")
-@click.option('-o', '--l2a_dir', help="Output directory")
-@click.option('-cfg', '--config', help="EOdag config file")
+@click.option('-o', '--l2a_dir',default=None, help="Output directory")
+@click.option('-cfg', '--config',default=None, help="EOdag config file")
 @click.option('-pv', '--provider', default="creodias", help="Data provider")
 def run_id(pid, l2a_dir, provider, config):
+    if l2a_dir is None:
+        l2a_dir = "/work/SEN2TEST/OUT/"
     # Generate temporary folders
     out_dir_l1c,out_dir_l2a = make_tmp_dirs(l2a_dir)
     # Get Sat product by id using eodag
@@ -73,6 +77,33 @@ def run_id(pid, l2a_dir, provider, config):
     clean(out_dir_l1c)
     # Send to s3
     ewoc_s3_upload()
+
+@cli.command('s2c_db', help="Sen2cor Postgreqsl mode")
+def run_db():
+    l2a_dir = "/work/SEN2TEST/OUT/"
+    # Generate temporary folders
+    out_dir_l1c,out_dir_l2a = make_tmp_dirs(l2a_dir)
+    # Get Sat product by id using eodag
+    ###### Insert ewoc_db get_id function here
+    pid = "S2 id from the get_id function"
+    get_product_by_id(pid, out_dir_l1c)
+    print(f'Download done for {pid}\n')
+    # Make sure to get the right path to the SAFE folder!
+    # TODO make this list comprehension more robust using regex
+    l1c_safe_folder = [os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith('SAFE')][0]
+    l1c_safe_folder = last_safe(l1c_safe_folder)
+    ## Processing time, here sen2cor, could be another processor
+    l2a_safe_folder = run_s2c(l1c_safe_folder, out_dir_l2a)
+    # Convert the sen2cor output to ewoc ard format
+    l2a_to_ard(l2a_safe_folder, l2a_dir)
+    # Delete local folders
+    clean(out_dir_l2a)
+    clean(out_dir_l1c)
+    # Send to s3
+    ewoc_s3_upload()
+    ###### Update status of id on success
+    # Insert update_status function here!
+
 
 
 if __name__ == "__main__":
