6275eddc0031bf7c7c98b477b658d1f8ce6b1ec5
Benatia Fahd <fahd.benatia@gmail.com>
2021-08-25 19:13:31 +0200

    Add logger and use it with click

--- run_s2c.py
+++ run_s2c.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 import click
 from dataship.dag.utils import get_product_by_id, l2a_to_ard
@@ -7,17 +6,27 @@ from ewoc_db.fill.update_status import get_next_tile
 
 from utils import *
 
+logger = logging.getLogger(__name__)
+
 
 @click.group()
-def cli():
+@click.option(
+    "--verbose",
+    type=click.Choice(["v", "vv"]),
+    default="vv",
+    help="Set verbosity level: v for info, vv for debug",
+    required=True,
+)
+def cli(verbose):
     click.secho("Run sen2cor", fg="green", bold=True)
+    set_logger(verbose)
 
 
-@cli.command('s2c_plan', help="Sen2cor with a full plan as input")
-@click.option('-p', '--plan', help="EWoC Plan in json format")
-@click.option('-o', '--l2a_dir', default=None,help="Output directory")
-@click.option('-cfg', '--config', default=None,help="EOdag config file")
-@click.option('-pv', '--provider', default=None, help="Data provider")
+@cli.command("s2c_plan", help="Sen2cor with a full plan as input")
+@click.option("-p", "--plan", help="EWoC Plan in json format")
+@click.option("-o", "--l2a_dir", default=None, help="Output directory")
+@click.option("-cfg", "--config", default=None, help="EOdag config file")
+@click.option("-pv", "--provider", default=None, help="Data provider")
 def run_plan(plan, l2a_dir, provider, config):
     if l2a_dir is None:
         l2a_dir = "/work/SEN2TEST/OUT/"
@@ -29,7 +38,7 @@ def run_plan(plan, l2a_dir, provider, config):
             os.makedirs(dem_tmp_dir)
         custom_s2c_dem(tile, tmp_dir=dem_tmp_dir)
         count = 0
-        prods = plan[tile]['S2_PROC']['INPUTS']
+        prods = plan[tile]["S2_PROC"]["INPUTS"]
         for prod in prods:
             out_dir_l1c = os.path.join(l2a_dir, "tmp_L1C", tile)
             out_dir_l2a = os.path.join(l2a_dir, "tmp_L2A", tile)
@@ -39,8 +48,12 @@ def run_plan(plan, l2a_dir, provider, config):
                 os.makedirs(out_dir_l2a)
             try:
                 get_product_by_id(prod, out_dir_l1c, provider, config_file=config)
-                print(f'Download done for {prod}\n')
-                l1c_safe_folder = [os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith('SAFE')][0]
+                logger.info(f"Download done for {prod}\n")
+                l1c_safe_folder = [
+                    os.path.join(out_dir_l1c, fold)
+                    for fold in os.listdir(out_dir_l1c)
+                    if fold.endswith("SAFE")
+                ][0]
                 l1c_safe_folder = last_safe(l1c_safe_folder)
                 l2a_safe_folder = run_s2c(l1c_safe_folder, out_dir_l2a)
                 l2a_to_ard(l2a_safe_folder, l2a_dir)
@@ -49,33 +62,34 @@ def run_plan(plan, l2a_dir, provider, config):
                 ewoc_s3_upload(l2a_dir)
                 count = +1
             except:
-                print(f'Something went wrong with {prod["id"]}')
+                logger.info(f'Something went wrong with {prod["id"]}')
         clean(dem_tmp_dir)
         number_of_products = len(prods)
-        print('\n\nEnd of processing ')
+        logger.info("\n\nEnd of processing ")
         # Check if
-        print(f'Processed {str(count)} of {str(number_of_products)}')
+        logger.info(f"Processed {str(count)} of {str(number_of_products)}")
 
 
-@cli.command('s2c_id', help="Sen2cor for on product using EOdag ID")
-@click.option('-p', '--pid', help="S2 L1C product ID")
-@click.option('-o', '--l2a_dir',default=None, help="Output directory")
-@click.option('-cfg', '--config',default=None, help="EOdag config file")
-@click.option('-pv', '--provider', default="creodias", help="Data provider")
+@cli.command("s2c_id", help="Sen2cor for on product using EOdag ID")
+@click.option("-p", "--pid", help="S2 L1C product ID")
+@click.option("-o", "--l2a_dir", default=None, help="Output directory")
+@click.option("-cfg", "--config", default=None, help="EOdag config file")
+@click.option("-pv", "--provider", default="creodias", help="Data provider")
 def run_id(pid, l2a_dir, provider, config):
     if l2a_dir is None:
         l2a_dir = "/work/SEN2TEST/OUT/"
     # Generate temporary folders
     dem_tmp_dir = "/work/SEN2TEST/DEM/"
-    tile = pid.split('_')[5][1:]
+    tile = pid.split("_")[5][1:]
     if not os.path.exists(dem_tmp_dir):
         os.makedirs(dem_tmp_dir)
     dem_syms = custom_s2c_dem(tile, tmp_dir=dem_tmp_dir)
-    out_dir_l1c,out_dir_l2a = make_tmp_dirs(l2a_dir)
+    out_dir_l1c, out_dir_l2a = make_tmp_dirs(l2a_dir)
     # Get Sat product by id using eodag
     get_product_by_id(pid, out_dir_l1c, provider, config_file=config)
-    l1c_safe_folder = \
-    [os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith('SAFE')][0]
+    l1c_safe_folder = [
+        os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith("SAFE")
+    ][0]
     l1c_safe_folder = last_safe(l1c_safe_folder)
     # Run sen2cor in subprocess
     l2a_safe_folder = run_s2c(l1c_safe_folder, out_dir_l2a)
@@ -89,27 +103,35 @@ def run_id(pid, l2a_dir, provider, config):
     # Send to s3
     ewoc_s3_upload(l2a_dir)
 
-@cli.command('s2c_db', help="Sen2cor Postgreqsl mode")
-@click.option('-e', '--executor', help="Name of the executor")
-@click.option('-f', '--status_filter', default="scheduled", help="Selects tiles that follow that condition")
+
+@cli.command("s2c_db", help="Sen2cor Postgreqsl mode")
+@click.option("-e", "--executor", help="Name of the executor")
+@click.option(
+    "-f",
+    "--status_filter",
+    default="scheduled",
+    help="Selects tiles that follow that condition",
+)
 def run_db(executor, status_filter):
     l2a_dir = "/work/SEN2TEST/OUT/"
     # Generate temporary folders
     dem_tmp_dir = "/work/SEN2TEST/DEM/"
     if not os.path.exists(dem_tmp_dir):
         os.makedirs(dem_tmp_dir)
-    out_dir_l1c,out_dir_l2a = make_tmp_dirs(l2a_dir)
+    out_dir_l1c, out_dir_l2a = make_tmp_dirs(l2a_dir)
     # Get Sat product by id using eodag
     db_type = "fsmac"
     tile, _ = get_next_tile(db_type, executor, status_filter)
     pid = tile.products
-    s2tile = pid.split('_')[5][1:]
+    s2tile = pid.split("_")[5][1:]
     custom_s2c_dem(s2tile, tmp_dir=dem_tmp_dir)
     get_product_by_id(pid, out_dir_l1c)
-    print(f'Download done for {pid}\n')
+    logger.info(f"Download done for {pid}\n")
     # Make sure to get the right path to the SAFE folder!
     # TODO make this list comprehension more robust using regex
-    l1c_safe_folder = [os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith('SAFE')][0]
+    l1c_safe_folder = [
+        os.path.join(out_dir_l1c, fold) for fold in os.listdir(out_dir_l1c) if fold.endswith("SAFE")
+    ][0]
     l1c_safe_folder = last_safe(l1c_safe_folder)
     ## Processing time, here sen2cor, could be another processor
     l2a_safe_folder = run_s2c(l1c_safe_folder, out_dir_l2a)
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+import logging
 import shutil
+import sys
 import zipfile
 
 import rasterio
@@ -6,8 +8,22 @@ from dataship.dag.s3man import *
 from eotile.eotile_module import main
 from rasterio.merge import merge
 
+logger = logging.getLogger(__name__)
 
-def run_s2c(l1c_safe,l2a_out):
+
+def set_logger(verbose_v):
+    """
+    Set the logger level
+    :param loglevel:
+    :return:
+    """
+    v_to_level = {"v": "INFO", "vv": "DEBUG"}
+    loglevel = v_to_level[verbose_v]
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
+
+
+def run_s2c(l1c_safe, l2a_out):
     """
     Run sen2cor subprocess
     :param l1c_safe: Path to SAFE folder
@@ -20,11 +36,15 @@ def run_s2c(l1c_safe,l2a_out):
     s2c_cmd = f"./Sen2Cor-02.09.00-Linux64/bin/L2A_Process {l1c_safe} --output_dir {l2a_out} --resolution 10"
     os.system(s2c_cmd)
     # TODO instead of getting the first element of this list, select folder using date and tile id from l1 id
-    l2a_safe_folder = [os.path.join(l2a_out, fold) for fold in os.listdir(l2a_out) if fold.endswith('SAFE')][0]
+    l2a_safe_folder = [os.path.join(l2a_out, fold) for fold in os.listdir(l2a_out) if fold.endswith("SAFE")][
+        0
+    ]
     return l2a_safe_folder
+
 
 def clean(folder):
     shutil.rmtree(folder)
+
 
 def last_safe(safe_folder):
     """
@@ -35,12 +55,14 @@ def last_safe(safe_folder):
     tmp = safe_folder
     for root, dirs, files in os.walk(safe_folder):
         for dir in dirs:
-            if dir.endswith('SAFE'):
-                tmp = os.path.join(root,dir)
+            if dir.endswith("SAFE"):
+                tmp = os.path.join(root, dir)
     return tmp
+
 
 def get_var_env(var_name):
     return os.getenv(var_name)
+
 
 def ewoc_s3_upload(local_path):
     try:
@@ -48,11 +70,13 @@ def ewoc_s3_upload(local_path):
         s3c = get_s3_client()
         bucket = get_var_env("BUCKET")
         s3_path = get_var_env("DEST_PREFIX")
-        recursive_upload_dir_to_s3(s3_client=s3c,local_path=local_path,s3_path=s3_path,bucketname=bucket)
+        recursive_upload_dir_to_s3(s3_client=s3c, local_path=local_path, s3_path=s3_path, bucketname=bucket)
         # <!> Delete output folder after upload
         clean(local_path)
+        logger.debug(f"{local_path} cleared")
     except:
-        print("Could not upload output folder to s3, results saved locally")
+        logger.info("Could not upload output folder to s3, results saved locally")
+
 
 def make_tmp_dirs(work_dir):
     out_dir_in = os.path.join(work_dir, "tmp_in")
@@ -61,59 +85,57 @@ def make_tmp_dirs(work_dir):
         os.makedirs(out_dir_in)
     if not os.path.exists(out_dir_proc):
         os.makedirs(out_dir_proc)
-    return out_dir_in,out_dir_proc
+    return out_dir_in, out_dir_proc
 
-def custom_s2c_dem(tile_id,tmp_dir):
-    srt_90 = main(tile_id,no_l8=True, no_s2=True, srtm5x5=True)
+
+def custom_s2c_dem(tile_id, tmp_dir):
+    srt_90 = main(tile_id, no_l8=True, no_s2=True, srtm5x5=True)
     srt_90 = srt_90[-1]
-    srtm_ids = list(srt_90['id'])
+    srtm_ids = list(srt_90["id"])
     bucket = "world-cereal"
-    srtm_list=[]
-    srtm_tiles=[]
+    srtm_list = []
+    srtm_tiles = []
     # Clear the srtm folder from tiles remaining from previous runs
     s2c_docker_srtm_folder = "/root/sen2cor/2.9/dem/srtm"
     clean(s2c_docker_srtm_folder)
+    logger.debug("/root/sen2cor/2.9/dem/srtm --> clean (deleted)")
     # Create (back) the srtm folder
     os.makedirs(s2c_docker_srtm_folder)
+    logger.debug("/root/sen2cor/2.9/dem/srtm --> created")
     # download the zip files
     for srtm_id in srtm_ids:
-        key = os.path.join("srtm90",srtm_id+".zip")
-        outzip = os.path.join(tmp_dir,srtm_id+".zip")
-        download_s3file(key,outzip,bucket)
-        outfold = outzip.replace('.zip','')
-        with zipfile.ZipFile(outzip, 'r') as zip_ref:
+        key = os.path.join("srtm90", srtm_id + ".zip")
+        outzip = os.path.join(tmp_dir, srtm_id + ".zip")
+        download_s3file(key, outzip, bucket)
+        outfold = outzip.replace(".zip", "")
+        with zipfile.ZipFile(outzip, "r") as zip_ref:
             zip_ref.extractall(outfold)
         os.remove(outzip)
-        srtm_list.append(os.path.join(tmp_dir,srtm_id,srtm_id+'.tif'))
+        srtm_list.append(os.path.join(tmp_dir, srtm_id, srtm_id + ".tif"))
         srtm_tiles.append(srtm_id)
     sources = []
-    output_fn = os.path.join(tmp_dir,f'mosaic_{"_".join(srtm_ids)}.tif')
+    output_fn = os.path.join(tmp_dir, f'mosaic_{"_".join(srtm_ids)}.tif')
     for raster in srtm_list:
         src = rasterio.open(raster)
         sources.append(src)
-    merge(sources,dst_path=output_fn,method='max')
+    merge(sources, dst_path=output_fn, method="max")
+    logger.debug(f"Created mosaic {output_fn}")
     for src in sources:
         src.close()
     links = []
     for tile in srtm_tiles:
         try:
-            os.symlink(output_fn, os.path.join(s2c_docker_srtm_folder, tile + '.tif'))
-            links.append(os.path.join(s2c_docker_srtm_folder, tile + '.tif'))
+            os.symlink(output_fn, os.path.join(s2c_docker_srtm_folder, tile + ".tif"))
+            links.append(os.path.join(s2c_docker_srtm_folder, tile + ".tif"))
         except OSError:
-            print('Symlink error: probably already exists')
+            logger.info("Symlink error: probably already exists")
     return links
+
 
 def unlink(links):
     for symlink in links:
         try:
             os.unlink(symlink)
-            print(f" -- [Ok] Unlinked {symlink}")
+            logger.info(f" -- [Ok] Unlinked {symlink}")
         except:
-            print(f"Cannot unlink {symlink}")
-
-
-
-
-
-
-
+            logger.info(f"Cannot unlink {symlink}")
