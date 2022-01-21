3a7c26d06aa6477a654c53803c2f6703a43a3d1a
Benatia Fahd <18417413+Fahdben@users.noreply.github.com>
2022-01-21 17:33:15 +0100

    Update s2c_plan command

--- src/ewoc_s2c/run_s2c.py
+++ src/ewoc_s2c/run_s2c.py
@@ -43,51 +43,70 @@ def cli(verbose):
 
 
 @cli.command("s2c_plan", help="Sen2cor with a full plan as input")
-@click.option("-p", "--plan", help="EWoC Plan in json format")
-@click.option("-o", "--l2a_dir", default=None, help="Output directory")
+@click.option("-p", "--plan", help="EWoC Workplan")
 @click.option(
     "--production_id",
     default="0000",
     help="Production ID that will be used to upload to s3 bucket. " "Default: 0000",
 )
-def run_plan(plan, l2a_dir, production_id):
-    if l2a_dir is None:
-        l2a_dir = "/work/SEN2TEST/OUT/"
-    dem_tmp_dir = "/work/SEN2TEST/DEM/"
+@click.option("-ds", "--data_source", default="creodias")
+@click.option("-sc", "--only_scl", default=False, is_flag=True)
+@click.option("--no_sen2cor", help="Do not process with Sen2cor", is_flag=True)
+def run_plan(plan, production_id, data_source, only_scl, no_sen2cor):
+    l2a_dir = Path("/work/SEN2TEST/OUT/")
+    l2a_dir.mkdir(exist_ok=True, parents=True)
     with open(plan) as file_plan:
         plan = json.load(file_plan)
     for tile in plan:
-        if not os.path.exists(dem_tmp_dir):
-            os.makedirs(dem_tmp_dir)
-        custom_s2c_dem(tile, tmp_dir=dem_tmp_dir)
+        dem_tmp_dir = Path("/work/SEN2TEST/DEM/")
+        dem_tmp_dir.mkdir(exist_ok=True, parents=True)
+        dem_syms = custom_s2c_dem(tile, tmp_dir=dem_tmp_dir)
         count = 0
         prods = plan[tile]["S2_PROC"]["INPUTS"]
-        for prod in prods:
-            out_dir_l1c = os.path.join(l2a_dir, "tmp_L1C", tile)
-            out_dir_l2a = os.path.join(l2a_dir, "tmp_L2A", tile)
-            if not os.path.exists(out_dir_l1c):
-                os.makedirs(out_dir_l1c)
-            if not os.path.exists(out_dir_l2a):
-                os.makedirs(out_dir_l2a)
+        for pid in prods:
+            upload_dir = l2a_dir / "upload"
+            upload_dir.mkdir(exist_ok=True, parents=True)
+            out_dir_l2a = l2a_dir / "tmp_L2A"
+            out_dir_l2a.mkdir(exist_ok=True, parents=True)
             try:
-                bucket = CreodiasBucket()
-                bucket.download_s2_prd(prod, Path(out_dir_l1c))
-                logger.info(f"Download done for {prod}\n")
-                l1c_safe_folder = [
-                    os.path.join(out_dir_l1c, fold)
-                    for fold in os.listdir(out_dir_l1c)
-                    if fold.endswith("SAFE")
-                ][0]
-                l1c_safe_folder = last_safe(l1c_safe_folder)
-                l2a_safe_folder = run_s2c(l1c_safe_folder, out_dir_l2a)
-                l2a_to_ard(l2a_safe_folder, l2a_dir)
-                clean(out_dir_l2a)
-                clean(out_dir_l1c)
-                ewoc_s3_upload(l2a_dir, production_id)
+                if "L2A" in pid and not no_sen2cor:
+                    raise AttributeError("Using L2A product with Sen2cor is impossible")
+
+                if no_sen2cor:
+                    if only_scl:
+                        scl_folder = get_s2_product(
+                            pid, l2a_dir, source=data_source, l2_mask_only=True
+                        )
+                        l2a_to_ard(scl_folder, l2a_dir, only_scl)
+                        ewoc_s3_upload(Path(upload_dir), production_id)
+                    else:
+                        raise NotImplementedError(
+                            "Only the SCL MASK production is implemented"
+                        )
+                else:
+                    # Get Sat product by id using ewoc_dag
+                    if data_source == "aws":
+                        l1c_safe_folder = get_s2_product(
+                            pid, l2a_dir, source=data_source, aws_l1c_safe=True
+                        )
+                    else:
+                        l1c_safe_folder = get_s2_product(
+                            pid, l2a_dir, source=data_source
+                        )
+                    # Run sen2cor in subprocess
+                    l2a_safe_folder = run_s2c(l1c_safe_folder, out_dir_l2a, only_scl)
+                    # Convert the sen2cor output to ewoc ard format
+                    l2a_to_ard(l2a_safe_folder, upload_dir, only_scl)
+                    # Delete local folders
+                    clean(out_dir_l2a)
+                    # Send to s3
+                    ewoc_s3_upload(Path(upload_dir), production_id)
+                    clean(upload_dir)
                 count = +1
             except RuntimeError:
-                logger.info(f'Something went wrong with {prod["id"]}')
+                logger.info(f"Something went wrong with {pid}")
         clean(dem_tmp_dir)
+        unlink(dem_syms)
         number_of_products = len(prods)
         logger.info("\n\nEnd of processing ")
         # Check if
@@ -133,10 +152,9 @@ def run_id(pid, production_id, data_source, only_scl=False, no_sen2cor=False):
             raise NotImplementedError("Only the SCL MASK production is implemented")
     else:
         # Generate temporary folders
-        dem_tmp_dir = "/work/SEN2TEST/DEM/"
+        dem_tmp_dir = Path("/work/SEN2TEST/DEM/")
+        dem_tmp_dir.mkdir(exist_ok=True, parents=True)
         tile = pid.split("_")[5][1:]
-        if not os.path.exists(dem_tmp_dir):
-            os.makedirs(dem_tmp_dir)
         dem_syms = custom_s2c_dem(tile, tmp_dir=dem_tmp_dir)
         out_dir_l1c, out_dir_l2a = make_tmp_dirs(l2a_dir)
         # Get Sat product by id using ewoc_dag
@@ -152,11 +170,11 @@ def run_id(pid, production_id, data_source, only_scl=False, no_sen2cor=False):
         l2a_to_ard(l2a_safe_folder, upload_dir, only_scl)
         # Delete local folders
         clean(out_dir_l2a)
-        clean(out_dir_l1c)
         clean(dem_tmp_dir)
         unlink(dem_syms)
         # Send to s3
         ewoc_s3_upload(Path(upload_dir), production_id)
+        clean(upload_dir)
 
 
 @cli.command("s2c_db", help="Sen2cor Postgreqsl mode")
