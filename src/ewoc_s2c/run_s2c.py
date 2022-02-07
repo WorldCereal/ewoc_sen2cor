60472aab157656c9d0d975104e4250c407cc6170
mbattude-cs <marjorie.battude@csgroup.eu>
2022-02-07 15:35:43 +0100

    Use pathlib and add typing

--- src/ewoc_s2c/run_s2c.py
+++ src/ewoc_s2c/run_s2c.py
@@ -54,12 +54,12 @@ def cli(verbose):
 @click.option("-ds", "--data_source", default="creodias")
 @click.option("-sc", "--only_scl", default=False, is_flag=True)
 @click.option("--no_sen2cor", help="Do not process with Sen2cor", is_flag=True)
-def run_plan(plan, production_id, data_source, only_scl, no_sen2cor):
+def run_plan(plan: str, production_id: str, data_source: str, only_scl: bool, no_sen2cor: bool):
     l2a_dir = Path("/work/SEN2TEST/OUT/")
     l2a_dir.mkdir(exist_ok=True, parents=True)
     with open(plan) as file_plan:
-        plan = json.load(file_plan)
-    tiles = plan["tiles"]
+        plan_new = json.load(file_plan)
+    tiles = plan_new["tiles"]
     for tile in tiles:
         dem_tmp_dir = Path("/work/SEN2TEST/DEM/")
         dem_tmp_dir.mkdir(exist_ok=True, parents=True)
@@ -128,7 +128,7 @@ def run_plan(plan, production_id, data_source, only_scl, no_sen2cor):
 @click.option("-ds", "--data_source", default="creodias")
 @click.option("-sc", "--only_scl", default=False, is_flag=True)
 @click.option("--no_sen2cor", help="Do not process with Sen2cor", is_flag=True)
-def run_id(pid, production_id, data_source, only_scl=False, no_sen2cor=False):
+def run_id(pid: str, production_id: str, data_source: str, only_scl: bool = False, no_sen2cor: bool = False):
     """
     Run Sen2Cor with a product ID
     :param pid: Sentinel-2 product identifier
@@ -154,7 +154,7 @@ def run_id(pid, production_id, data_source, only_scl=False, no_sen2cor=False):
                 pid, l2a_dir, source=data_source, l2_mask_only=True
             )
             l2a_to_ard(scl_folder, l2a_dir, only_scl)
-            ewoc_s3_upload(Path(upload_dir), production_id)
+            ewoc_s3_upload(upload_dir, production_id)
         else:
             raise NotImplementedError("Only the SCL MASK production is implemented")
     else:
@@ -180,7 +180,7 @@ def run_id(pid, production_id, data_source, only_scl=False, no_sen2cor=False):
         clean(dem_tmp_dir)
         unlink(dem_syms)
         # Send to s3
-        ewoc_s3_upload(Path(upload_dir), production_id)
+        ewoc_s3_upload(upload_dir, production_id)
 
 
 @cli.command("s2c_db", help="Sen2cor Postgreqsl mode")
@@ -197,11 +197,10 @@ def run_id(pid, production_id, data_source, only_scl=False, no_sen2cor=False):
     help="Production ID that will be used to upload to s3 bucket. " "Default: 0000",
 )
 def run_db(executor, status_filter, production_id):
-    l2a_dir = "/work/SEN2TEST/OUT/"
+    l2a_dir = Path("/work/SEN2TEST/OUT/")
     # Generate temporary folders
-    dem_tmp_dir = "/work/SEN2TEST/DEM/"
-    if not os.path.exists(dem_tmp_dir):
-        os.makedirs(dem_tmp_dir)
+    dem_tmp_dir = Path("/work/SEN2TEST/DEM/")
+    dem_tmp_dir.mkdir(exist_ok=False, parents=True)
     out_dir_l1c, out_dir_l2a = make_tmp_dirs(l2a_dir)
     # Get Sat product by id using eodag
     db_type = "fsmac"
@@ -215,7 +214,7 @@ def run_db(executor, status_filter, production_id):
     # Make sure to get the right path to the SAFE folder!
     # TODO make this list comprehension more robust using regex
     l1c_safe_folder = [
-        os.path.join(out_dir_l1c, fold)
+        out_dir_l1c / fold
         for fold in os.listdir(out_dir_l1c)
         if fold.endswith("SAFE")
     ][0]
--- src/ewoc_s2c/utils.py
+++ src/ewoc_s2c/utils.py
@@ -6,6 +6,7 @@ import signal
 import sys
 from contextlib import ContextDecorator
 from pathlib import Path
+from typing import List, Optional, Tuple 
 
 import boto3.exceptions
 import numpy as np
@@ -20,7 +21,7 @@ logger = logging.getLogger(__name__)
 TIMEOUT_SECONDS = 900
 
 
-def binary_scl(scl_file, raster_fn):
+def binary_scl(scl_file: Path, raster_fn: Path):
     """
     Convert L2A SCL file to binary cloud mask
     :param scl_file: Path to SCL file
@@ -58,7 +59,7 @@ def binary_scl(scl_file, raster_fn):
         out.write(mask.astype(rasterio.uint8), 1)
 
 
-def scl_to_ard(work_dir, prod_name):
+def scl_to_ard(work_dir: Path, prod_name: str):
     """
     Convert the SCL L2A product into EWoC ARD format
     :param work_dir: Output directory
@@ -74,32 +75,23 @@ def scl_to_ard(work_dir, prod_name):
     tile_id = product_id.split("_")[5][1:]
     atcor_algo = "L2A"
     unique_id = "".join(product_id.split("_")[3:6])
-    folder_st = os.path.join(
-        work_dir,
-        "OPTICAL",
-        tile_id[:2],
-        tile_id[2],
-        tile_id[3:],
-        year,
-        date.split("T")[0],
-    )
+    folder_st = work_dir / "OPTICAL" / tile_id[:2] / tile_id[2] / tile_id[3:] / year / date.split("T")[0]
     dir_name = f"{platform}_{processing_level}_{date}_{unique_id}_{tile_id}"
-    tmp_dir = os.path.join(folder_st, dir_name)
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
+    tmp_dir = folder_st / dir_name
+    tmp_dir.mkdir(exist_ok=False, parents=True)
 
     out_cld = f"{platform}_{atcor_algo}_{date}_{unique_id}_{tile_id}_MASK.tif"
-    raster_cld = os.path.join(folder_st, dir_name, out_cld)
-    input_file = str(Path(work_dir) / (prod_name + ".tif"))
+    raster_cld = folder_st / dir_name / out_cld
+    input_file = work_dir / prod_name / ".tif"
     binary_scl(input_file, raster_cld)
     try:
-        os.remove(input_file)
-        os.remove(raster_cld + ".aux.xml")
+        input_file.unlink()
+        (raster_cld / ".aux.xml").unlink()
     except FileNotFoundError:
         logger.info("Clean")
 
 
-def l2a_to_ard(l2a_folder, work_dir, only_scl=False):
+def l2a_to_ard(l2a_folder: Path, work_dir: Path, only_scl: bool = False)-> Path:
     """
     Convert an L2A product into EWoC ARD format
     :param l2a_folder: L2A SAFE folder
@@ -133,47 +125,38 @@ def l2a_to_ard(l2a_folder, work_dir, only_scl=False):
     tile_id = product_id.split("_")[5][1:]
     atcor_algo = "L2A"
     unique_id = "".join(product_id.split("_")[3:6])
-    folder_st = os.path.join(
-        work_dir,
-        "OPTICAL",
-        tile_id[:2],
-        tile_id[2],
-        tile_id[3:],
-        year,
-        date.split("T")[0],
-    )
+    folder_st = work_dir / "OPTICAL" / tile_id[:2] / tile_id[2] / tile_id[3:] / year / date.split("T")[0]
     dir_name = f"{platform}_{processing_level}_{date}_{unique_id}_{tile_id}"
-    tmp_dir = os.path.join(folder_st, dir_name)
-    ard_folder = os.path.join(folder_st, dir_name)
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
+    tmp_dir = folder_st / dir_name
+    ard_folder = folder_st / dir_name
+    tmp_dir.mkdir(exist_ok=False, parents=True)
 
     # Convert bands and SCL
     for band in bands:
         res = bands[band]
         band_path = find_l2a_band(l2a_folder, band, bands[band])
-        band_name = os.path.split(band_path)[-1]
+        band_name = band_path.parts[-1]
         band_name = band_name.replace(".jp2", ".tif").replace(f"_{str(res)}m", "")
         logger.info("Processing band " + band_name)
         out_name = f"{platform}_{atcor_algo}_{date}_{unique_id}_{tile_id}_{band}.tif"
-        raster_fn = os.path.join(folder_st, dir_name, out_name)
+        raster_fn = folder_st / dir_name / out_name
         if band == "SCL":
             out_cld = f"{platform}_{atcor_algo}_{date}_{unique_id}_{tile_id}_MASK.tif"
-            raster_cld = os.path.join(folder_st, dir_name, out_cld)
+            raster_cld = folder_st / dir_name / out_cld
             binary_scl(band_path, raster_cld)
-            logger.info("Done --> " + raster_cld)
+            logger.info("Done --> " + str(raster_cld))
             try:
-                os.remove(raster_cld + ".aux.xml")
+                (raster_cld / ".aux.xml").unlink()
             except FileNotFoundError:
                 logger.info("Clean")
 
         else:
             raster_to_ard(band_path, band, raster_fn)
-            logger.info("Done --> " + raster_fn)
+            logger.info("Done --> " + str(raster_fn))
     return ard_folder
 
 
-def set_logger(verbose_v):
+def set_logger(verbose_v: str):
     """
     Set the logger level
     :param loglevel:
@@ -200,7 +183,7 @@ class TimeOut(ContextDecorator):
     Time out decorator
     """
 
-    def __init__(self, secs):
+    def __init__(self, secs: float):
         self.seconds = secs
 
     def _handle_timeout(self, signum, frame):
@@ -215,11 +198,11 @@ class TimeOut(ContextDecorator):
 
 
 def run_s2c(
-    l1c_safe,
-    l2a_out,
-    only_scl=False,
-    bin_path="./Sen2Cor-02.09.00-Linux64/bin/L2A_Process",
-):
+    l1c_safe: Path,
+    l2a_out: Path,
+    only_scl: bool = False,
+    bin_path: str = "./Sen2Cor-02.09.00-Linux64/bin/L2A_Process",
+)->Path:
     """
     Run sen2cor subprocess
     :param l1c_safe: Path to SAFE folder
@@ -238,14 +221,14 @@ def run_s2c(
     os.system(s2c_cmd)
     # TODO: select folder using date and tile id from l1 id
     l2a_safe_folder = [
-        os.path.join(l2a_out, fold)
+        l2a_out / fold
         for fold in os.listdir(l2a_out)
         if fold.endswith("SAFE")
     ][0]
     return l2a_safe_folder
 
 
-def clean(folder):
+def clean(folder: Path):
     """
     Delete folder recursively
     :param folder: Path to folder to be deleted
@@ -254,21 +237,21 @@ def clean(folder):
     shutil.rmtree(folder)
 
 
-def last_safe(safe_folder):
+def last_safe(safe_folder: Path)->Path:
     """
     Get the deepest/last SAFE folder when there are many
     :param safe_folder: path to .SAFE folder
     :return: last path
     """
     tmp = safe_folder
-    for root, dirs, _ in os.walk(safe_folder):
+    for root, dirs, _ in walk(safe_folder):
         for dir_ in dirs:
             if dir_.endswith("SAFE"):
-                tmp = os.path.join(root, dir_)
+                tmp = root / dir
     return tmp
 
 
-def ewoc_s3_upload(local_path, ard_prd_prefix):
+def ewoc_s3_upload(local_path: Path, ard_prd_prefix: str):
     """
     Upload file to the Cloud (S3 bucket)
     :param local_path: Path to the file to be uploaded
@@ -289,37 +272,37 @@ def ewoc_s3_upload(local_path, ard_prd_prefix):
         logger.info("Could not upload output folder to s3, results saved locally")
 
 
-def init_folder(folder_path):
+def init_folder(folder_path: Path):
     """
     Create some work folders, delete if existing
     :param folder_path: Path to folders location
     :return: None
     """
-    if os.path.exists(folder_path):
+    if folder_path.is_dir():
         logger.info(f"Found {folder_path} -- start reset")
         clean(folder_path)
         logger.info(f"Cleared {folder_path}")
-        os.makedirs(folder_path)
+        folder_path.mkdir(exist_ok=False, parents=True)
         logger.info(f"Created new folder {folder_path}")
     else:
-        os.makedirs(folder_path)
+        folder_path.mkdir(exist_ok=False, parents=True)
         logger.info(f"Created new folder {folder_path}")
 
 
-def make_tmp_dirs(work_dir):
+def make_tmp_dirs(work_dir: Path)->Tuple[Path, Path]:
     """
     Crearte folders
     :param work_dir: folders location
     :return: None
     """
-    out_dir_in = os.path.join(work_dir, "tmp_in")
-    out_dir_proc = os.path.join(work_dir, "tmp_proc")
+    out_dir_in = work_dir / "tmp_in"
+    out_dir_proc = work_dir / "tmp_proc"
     init_folder(out_dir_in)
     init_folder(out_dir_proc)
     return out_dir_in, out_dir_proc
 
 
-def custom_s2c_dem(tile_id, tmp_dir):
+def custom_s2c_dem(tile_id: str, tmp_dir: Path)->List:
     """
     Download and create an srtm mosaïc
     :param tile_id: MGRS tile id (ex 31TCJ Toulouse)
@@ -330,21 +313,21 @@ def custom_s2c_dem(tile_id, tmp_dir):
     srt_90 = srt_90[-1]
     srtm_ids = list(srt_90["id"])
     # Clear the srtm folder from tiles remaining from previous runs
-    s2c_docker_srtm_folder = "/root/sen2cor/2.9/dem/srtm"
+    s2c_docker_srtm_folder = Path("/root/sen2cor/2.9/dem/srtm")
     clean(s2c_docker_srtm_folder)
     logger.info("/root/sen2cor/2.9/dem/srtm --> clean (deleted)")
     # Create (back) the srtm folder
-    os.makedirs(s2c_docker_srtm_folder)
+    s2c_docker_srtm_folder.mkdir(exist_ok=False, parents=True)
     logger.info("/root/sen2cor/2.9/dem/srtm --> created")
     # download the zip files
     bucket = EWOCAuxDataBucket()
-    bucket.download_srtm3s_tiles(srtm_ids, Path(tmp_dir))
+    bucket.download_srtm3s_tiles(srtm_ids, tmp_dir)
 
     sources = []
-    output_fn = os.path.join(tmp_dir, f'mosaic_{"_".join(srtm_ids)}.tif')
+    output_fn = tmp_dir / f'mosaic_{"_".join(srtm_ids)}.tif'
 
     for srtm_id in srtm_ids:
-        raster_name = os.path.join(tmp_dir, "srtm3s", srtm_id + ".tif")
+        raster_name = tmp_dir / "srtm3s", srtm_id + ".tif"
         src = rasterio.open(raster_name)
         sources.append(src)
     merge(sources, dst_path=output_fn, method="max")
@@ -354,14 +337,14 @@ def custom_s2c_dem(tile_id, tmp_dir):
     links = []
     for tile in srtm_ids:
         try:
-            os.symlink(output_fn, os.path.join(s2c_docker_srtm_folder, tile + ".tif"))
-            links.append(os.path.join(s2c_docker_srtm_folder, tile + ".tif"))
+            (s2c_docker_srtm_folder / tile / ".tif").symlink_to(output_fn)
+            links.append(s2c_docker_srtm_folder / tile / ".tif")
         except OSError:
             logger.info("Symlink error: probably already exists")
     return links
 
 
-def unlink(links):
+def unlink(links: List):
     """
     Remove symlinks created
     :param links: List of links
@@ -369,7 +352,16 @@ def unlink(links):
     """
     for symlink in links:
         try:
-            os.unlink(symlink)
+            symlink.unlink()
             logger.info(f" -- [Ok] Unlinked {symlink}")
         except FileNotFoundError:
             logger.info(f"Cannot unlink {symlink}")
+
+
+def walk(path: Path): 
+    for p in path.iterdir(): 
+        if p.is_dir(): 
+            yield from walk(p)
+            continue
+        yield p.resolve()
+
